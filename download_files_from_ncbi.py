8560e9686d53b222748636795ea4ddf0658e4442
Esty Katzeff <esty.katzeff@gmail.com>
2022-02-09 11:59:17 +0200

    code works for downloading all bacteria assemblies + report stats into local folder

--- download_files_from_ncbi.py
+++ download_files_from_ncbi.py
@@ -1,11 +1,11 @@
 #https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Xanthomonas_oryzae/representative/GCF_001021915.1_ASM102191v1/
-
+# next step - option to download only for specific bacteria
 from ftplib import FTP
 import os, sys, os.path
 
 SERVER_PATH = r"ftp.ncbi.nlm.nih.gov"
 SERVER_BACTERIA_LOCATION = r"/genomes/refseq/bacteria/"
-SAVE_FOLDER = r"C:\Users\97252\Documents\year_4\bio_project_data\web server\results"
+SAVE_FOLDER = r"C:\Users\97252\Documents\year_4\bio_project_data\download_files_from_ncbi"
 
 ALL_ASSEMBLY_PREFIX = r"latest_assembly_versions/"
 NUMBER_OF_TRIES = 500
@@ -36,37 +36,45 @@ def get_genomes(num_try: int):
         # print(local_folder)
 
         os.makedirs(local_folder, exist_ok=True) #I think makedir is enough (doens't need to be makedirs)
-        # if file already downloaded
-        if os.listdir(local_folder):
-            # print(f'already downloaded {bacteria}')
-            num_already_downloaded += 1
-            continue
+
 
         file = None
         try:
             all_assembly_folder = bacteria + "/" + ALL_ASSEMBLY_PREFIX
-            # each folder may contain many assembyies of same bacteria
+            # each folder may contain many assemblies of same bacteria
             assemblies = ftp.nlst(all_assembly_folder)
-            for j in range(len(assemblies)):
-                files = ftp.nlst(assemblies[j])
-                try:
-                    file2download = f'{assemblies[0]}/{os.path.split(assemblies[0])[-1]}_genomic.fna.gz' #in order to also download the stats I will need to add here a download of _assembly_stats.txt
-                    local_filename = os.path.join(local_folder, os.path.basename(os.path.normpath(file2download)))
-                    # wget.download(file2download, local_filename)
-                    file = open(local_filename, 'wb')
-                    ftp.retrbinary('RETR ' + file2download, file.write)
-                    print(f'finished downloading file {local_filename}')
-                    num_errors_in_try = 0
-                except:
-                    print(f'error downloading {assemblies[0]} of bacteria {bacteria}')
+            if len([name for name in os.listdir(local_folder) if os.path.isfile(os.path.join(local_folder, name)) and (name.endswith("_genomic.fna.gz") or name.endswith("_assembly_stats.txt"))])/2 < len(assemblies):
+                for j in range(len(assemblies)):
+                    files = ftp.nlst(assemblies[j])
+                    try: #Maybe: to save time - I can check if the file exists before downloading
+                        file2download = f'{assemblies[j]}/{os.path.split(assemblies[j])[-1]}_genomic.fna.gz'
+                        local_filename = os.path.join(local_folder, os.path.basename(os.path.normpath(file2download)))
+                        file = open(local_filename, 'wb')
+                        ftp.retrbinary('RETR ' + file2download, file.write)
+                        print(f'finished downloading file {local_filename}')
+                        num_errors_in_try = 0
+                    except:
+                        print(f'error downloading fasta file for {assemblies[j]} of bacteria {bacteria}')
+                    if file != None:
+                        file.close()
+                    try:
+                        file2download = f'{assemblies[j]}/{os.path.split(assemblies[j])[-1]}_assembly_stats.txt'
+                        local_filename = os.path.join(local_folder, os.path.basename(os.path.normpath(file2download)))
+                        file = open(local_filename, 'wb')
+                        ftp.retrbinary('RETR ' + file2download, file.write)
+                        print(f'finished downloading file {local_filename}')
+                        num_errors_in_try = 0
+                    except:
+                        print(f'error downloading stats file for {assemblies[j]} of bacteria {bacteria}')
+                    if file != None:
+                        file.close()
         except:
             num_errors_in_try += 1
             print(f'error downloading bacteria {bacteria}')
             if num_errors_in_try >= MAX_NUM_ERRORS_IN_TRY:
                 break
 
-        if file != None:
-            file.close()
+
 
     print(f'num_try {num_try}, already_downloaded {num_already_downloaded} | len {len(bacteria_folders)}')
     ftp.quit()  # This is the “polite” way to close a connection
